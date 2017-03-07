8a655dcf96e1c44c55f4f7128fa8a50deec1e832
hoanpk <hoantkhd@gmail.com>
2017-03-07 20:51:52 +0700

    buildStructure

---
+++ WebApplication1/WebApplication1/login.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="login.aspx.vb" Inherits="WebApplication1.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="username"></asp:Label>
+        <asp:TextBox ID="username" runat="server" Height="16px" Width="206px"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="password"></asp:Label>
+            <asp:TextBox ID="password" runat="server" TextMode="Password" Width="207px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="login" Width="75px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/login.aspx.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Data.SqlClient
+Public Class login
+
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    End Sub
+
+    Private _Connectionstring As String = ConfigurationSettings.AppSettings("ConnStringQLSV")
+    Private conn As SqlConnection
+    Private da As SqlDataAdapter
+    Private Function getDataTable(sqlQuery As String) As DataTable
+        Dim dTable As New DataTable
+        conn = New SqlConnection(_Connectionstring)
+        da = New SqlDataAdapter(sqlQuery, conn)
+        Try
+            conn.Open()
+            da.Fill(dTable)
+        Catch ex As Exception
+            WriteLine(ex.Message, "Error")
+            Throw ex
+        Finally
+            conn.Close()
+        End Try
+        Return dTable
+    End Function
+    Public Function CheckLogin(user As String, pass As String) As Boolean
+        Dim sqlQuery As String = String.Format("select * from dbo.account where username = '{0}' and password = '{1}'", user, pass)
+        Dim dTable As DataTable = getDataTable(sqlQuery)
+        Return dTable.Rows.Count > 0
+    End Function
+
+
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If CheckLogin(Me.username.Text.ToLower, Me.password.Text.ToLower) Then
+            WriteLine("dang nhap thanh cong")
+
+        End If
+    End Sub
+End Class
